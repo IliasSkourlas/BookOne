3486f24b7f0851a76d605b4c83e7f20e972eeb91
Ilias Skourlas <penelopipenelopis@gmail.com>
2019-03-24 02:05:58 +0200

    On -Welcome and- About

--- BookOne/BookOne/Controllers/HomeController.cs
+++ BookOne/BookOne/Controllers/HomeController.cs
@@ -15,10 +15,11 @@ namespace BookOne.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Welcome. This is an app for searing your books with your friends. Not digital books, but your physical books. " +
-                 "It helps you to keep track with their whereabouts, as they are seared from a pool of carriers that you choose. " +
-                 "Read and write reviews and decide to give or not a Clap, when they are returned back to you in perfect condition. " +
-                 "So...explore and have fun...and don't hesitate searing!";
+            ViewBag.Message = "Welcome, we are aiming on rediscovering the world of your books.";
+                //"This is an app for searing your books with your friends. Not digital books, but your physical books. " +
+                // "It helps you to keep track with their whereabouts, as they are seared from a pool of carriers that you choose. " +
+                // "Read and write reviews and decide to give or not a Clap, when they are returned back to you in perfect condition. " +
+                // "So...explore and have fun...and don't hesitate searing!";
 
             return View();
         }
